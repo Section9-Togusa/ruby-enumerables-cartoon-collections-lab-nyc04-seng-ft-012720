1e8dc6490e5fa27e23d2baa6277d45317a4b857d
Daryl Corbin <corbindac@gmail.com>
2020-01-19 18:04:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,3 +11,17 @@ end
 end
 
 roll_call_dwarves(dwarves)
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(planeteer)
+
+planeteer.collect do |call|
+
+call.capitalize + "!"
+
+end
+
+end
+
+summon_captain_planet(planeteer_calls)
