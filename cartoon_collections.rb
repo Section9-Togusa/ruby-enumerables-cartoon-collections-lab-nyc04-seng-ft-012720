0fcda947788ea1aae3a49d6c01af428930c55ca4
Daryl Corbin <corbindac@gmail.com>
2020-01-19 18:04:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,3 +25,21 @@ end
 end
 
 summon_captain_planet(planeteer_calls)
+
+short_words = ["puff", "go", "two"]
+
+#assorted_words = ["two", "go", "industrious", "bop"]
+
+def long_planeteer_calls(planetee)
+
+planetee.any? do |call|
+
+call.length > 4
+
+end
+
+end
+
+long_planeteer_calls(short_words)
+
+#long_planeteer_calls(assorted_words)
